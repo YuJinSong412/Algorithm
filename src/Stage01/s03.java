3fa48230f0488e88235c29db4fd0c5d24eabb31e
yujin <yujin@naver.com>
2018-02-26 19:27:37 +0900

    s03

---
+++ src/Stage01/s03.java
@@ -0,0 +1,16 @@
+package Stage01;
+import java.util.*;
+
+public class s03 {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		
+		int x = scanner.nextInt();
+		int y = scanner.nextInt();
+		
+		System.out.println(x-y);
+
+	}
+
+}
