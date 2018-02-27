0df7a9aff391ef0397f2b62312dcb4415cec5283
yujin <yujin@naver.com>
2018-02-27 11:45:11 +0900

    s01

--- src/Stage02/s01.java
+++ src/Stage02/s01.java
@@ -1,9 +1,15 @@
 package Stage02;
+import java.util.*;
 
 public class s01 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner scanner = new Scanner(System.in);
+		
+		int x = scanner.nextInt();
+		int y = scanner.nextInt();
+		
+		System.out.println(x+y);
 
 	}
 
