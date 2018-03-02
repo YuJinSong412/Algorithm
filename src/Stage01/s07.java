bbb515f9bfc8e9208e9dd2a06c298ba316195df8
yujin <yujin@naver.com>
2018-03-02 15:38:14 +0900

    s07

---
+++ src/Stage01/s07.java
@@ -0,0 +1,37 @@
+package Stage01;
+
+import java.util.Scanner;
+
+public class s07 {
+
+	public static void main(String[] args) {
+
+		Scanner scanner = new Scanner(System.in);
+
+		int j = 0;
+		while (scanner.hasNextLine()) {
+			if (j == 100)
+				break;
+			String text = scanner.nextLine();
+
+			if (text.startsWith(" ") || text.endsWith(" "))
+				break;
+			if (text.length() > 100)
+				break;
+
+			for (int i = 0; i < text.length(); i++) {
+				if (text.charAt(i) < 97 && text.charAt(i) > 122) {
+					break;
+				}
+				if (text.charAt(i) < 65 && text.charAt(i) > 90) {
+					break;
+				}
+				if (text.charAt(i) < 48 && text.charAt(i) > 57) {
+					break;
+				}
+			}
+				System.out.println(text);
+				j++;
+			}
+		}
+	}
