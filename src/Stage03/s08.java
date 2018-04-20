92cc1a9c69bfb39144e5da2341921ff6624cafd7
yujin <yujin@naver.com>
2018-04-20 18:34:57 +0900

    s08

--- src/Stage03/s08.java
+++ src/Stage03/s08.java
@@ -1,9 +1,13 @@
 package Stage03;
+import java.util.*;
 
+enum Week{
+	MON,TUE,WED,THU,FRI,SAT,SUN;
+}
 public class s08 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 
 	}
 
