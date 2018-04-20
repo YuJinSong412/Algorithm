750895d07b87548e9dc0e94ffff98a961d529721
yujin <yujin@naver.com>
2018-04-20 20:19:40 +0900

    s02

---
+++ src/Stage04/s02.java
@@ -0,0 +1,31 @@
+package Stage04;
+import java.util.*;
+
+public class s02 {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		
+		int max = 0,mid =0;
+		
+		int num[] = new int[3];
+		for(int i=0;i<3;i++) {
+			num[i] = scanner.nextInt();
+		}
+		
+		max = num[0];
+		for(int i=1;i<3;i++) {
+			if(max <num[i]) {
+				mid = max;
+				max = num[i];			
+			}
+			else if(max == num[i])
+				mid = num[i];
+			else if(max > num[i] )
+				mid = num[i];
+		}
+		
+		System.out.println(mid);
+	}
+	
+}
