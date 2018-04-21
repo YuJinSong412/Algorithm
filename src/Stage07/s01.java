8183efc093755e32aa8eab8fa27b582f76562f1b
yujin <yujin@naver.com>
2018-04-21 23:06:41 +0900

    Stage07

---
+++ src/Stage07/s01.java
@@ -0,0 +1,15 @@
+package Stage07;
+import java.util.*;
+
+public class s01 {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		String a = scanner.next();
+		
+		int b = a.charAt(0);
+		System.out.println(b);
+		
+	}
+
+}
