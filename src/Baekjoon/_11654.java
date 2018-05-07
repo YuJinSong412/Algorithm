6bc2cbd2583246c3a1bbd6731d94dcc2ef5f4212
yujin <yujin@naver.com>
2018-05-07 22:31:48 +0900

    11654

---
+++ src/Baekjoon/_11654.java
@@ -0,0 +1,17 @@
+package Baekjoon;
+import java.util.*;
+
+public class _11654 {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		
+		char a = scanner.nextLine().charAt(0);
+		
+		System.out.println((int)a);
+				
+		
+
+	}
+
+}
