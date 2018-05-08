106b74399e32eaccf704cc07dbbe763a109743d7
yujin <yujin@naver.com>
2018-05-08 17:22:09 +0900

    2163

---
+++ src/Baekjoon/_2163.java
@@ -0,0 +1,17 @@
+package Baekjoon;
+import java.util.*;
+
+public class _2163 {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		
+		int a = scanner.nextInt();
+		int b = scanner.nextInt();
+		
+		int result = a*b;
+		System.out.println(result-1);
+
+	}
+
+}
