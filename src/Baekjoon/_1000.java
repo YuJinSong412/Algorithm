766e15143b8ae20544d16eed8544c484d4905b3e
yujin <yujin@naver.com>
2018-06-16 14:59:21 +0900

    1000

---
+++ src/Baekjoon/_1000.java
@@ -0,0 +1,14 @@
+package Baekjoon;
+import java.util.*;
+
+public class _1000 {
+	
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		int x = scanner.nextInt();
+		int y = scanner.nextInt();
+		
+		System.out.println(x+y);
+	}
+
+}
