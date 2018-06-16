7de18bf50c73cb5b009093ac0839157e0b4f56e5
yujin <yujin@naver.com>
2018-06-16 14:59:30 +0900

    10172

---
+++ src/Baekjoon/_10172.java
@@ -0,0 +1,13 @@
+package Baekjoon;
+
+public class _10172 {
+
+	public static void main(String[] args) {
+		System.out.println("|\\_/|");
+		System.out.println("|q p|   /}");
+		System.out.println("( 0 )\"\"\"\\");
+		System.out.println("|\"^\"`    |");
+		System.out.println("||_/=\\\\__|");
+
+	}
+}
