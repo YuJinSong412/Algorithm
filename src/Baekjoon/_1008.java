45141b2836a1acdc8b965d7cb89974e7ee7b9b87
yujin <yujin@naver.com>
2018-07-05 23:21:32 +0900

    1008

---
+++ src/Baekjoon/_1008.java
@@ -0,0 +1,15 @@
+package Baekjoon;
+import java.util.*;
+
+public class _1008 {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		int a = scanner.nextInt();
+		int b = scanner.nextInt();
+		
+		System.out.println((double)a/b);
+
+	}
+
+}
