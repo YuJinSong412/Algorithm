23e96568b3f01821d62c596c7a69a7de5ffc7bd7
yujin <yujin@naver.com>
2018-07-16 16:19:45 +0900

    15894

---
+++ src/Baekjoon/_15894.java
@@ -0,0 +1,21 @@
+package Baekjoon;
+import java.util.*;
+
+public class _15894 {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		
+		int num = scanner.nextInt();
+		
+		/* method_2
+		int first = 1;
+		int side = num * 2;
+		int last = num-1;
+		
+		int sum = first + num + side + last;
+		System.out.println(sum);
+		*/
+		System.out.println(num*4);
+	}
+}
