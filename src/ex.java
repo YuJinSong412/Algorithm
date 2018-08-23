1fd395e9d48117ca63cc1165eb68b0c91b2a6955
yujin <yujin@naver.com>
2018-08-23 16:22:21 +0900

    D

--- src/ex.java
+++ src/ex.java
@@ -3,16 +3,13 @@ import java.util.*;
 public class ex {
 
 	public static void main(String[] args) {
-		for(int i=0;i<5;i++) {
-			for(int j=4;j>i;j--) {
-				System.out.print(" ");
-			}
-			for(int j=1;j<=2*i+1;j++) {
-				System.out.print("*");
-			}
-			System.out.println();
+		int i=2;
+		
+		if (i==none) {
+			
 		}
 		
+
 	}
 
 }
