f5ea6f95906af0afc316626a2afb7619344cd1fd
yujin <yujin@naver.com>
2018-09-24 22:55:58 +0900

    '

---
+++ src/ex.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class ex {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		double num = (double)40/(double)80;
+		System.out.println(num);
+
+	}
+
+}
