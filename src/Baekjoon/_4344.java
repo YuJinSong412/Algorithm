68cd16e22be536ea010850b155052658b62054b3
yujin <yujin@naver.com>
2018-09-26 17:30:50 +0900

    d

---
+++ src/Baekjoon/_4344.java
@@ -0,0 +1,20 @@
+package Baekjoon;
+import java.util.*;
+
+public class _4344 {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		
+		int num = scanner.nextInt();
+		int[][] arr = new int[5][];
+		
+		for(int i=0;i<arr.length;i++) {
+			for(int j=0;j<arr[i].length;i++) {
+				arr[i][j]=scanner.nextInt();
+			}
+		}
+
+	}
+
+}
