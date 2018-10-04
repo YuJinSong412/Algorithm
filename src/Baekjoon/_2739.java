3488e12376d17a6968b1faaf3d176f651172992e
yujin <yujin@naver.com>
2018-10-04 19:52:59 +0900

    2738

---
+++ src/Baekjoon/_2739.java
@@ -0,0 +1,16 @@
+package Baekjoon;
+import java.util.*;
+
+public class _2739 {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		int N = scanner.nextInt();
+		
+		for(int i=1;i<=9;i++) {
+			System.out.println(N+" * "+i + " = "+N*i);
+		}
+
+	}
+
+}
