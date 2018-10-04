816dee28c9e35db761a628859c0ecb408f116d1d
yujin <yujin@naver.com>
2018-10-04 19:52:14 +0900

    2438

---
+++ src/Baekjoon/_2438.java
@@ -0,0 +1,17 @@
+package Baekjoon;
+import java.util.*;
+
+public class _2438 {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		int N = scanner.nextInt();
+		
+		for(int i=0;i<N;i++) {
+			for(int j=0;j<=i;j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+}
