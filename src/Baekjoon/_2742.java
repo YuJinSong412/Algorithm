5cc67dec94e1625e7e620b80f0bdb11930912fcd
yujin <yujin@naver.com>
2018-10-04 19:54:51 +0900

    _2742

---
+++ src/Baekjoon/_2742.java
@@ -0,0 +1,16 @@
+package Baekjoon;
+import java.util.*;
+
+public class _2742 {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		int N = scanner.nextInt();
+		
+		for(int i=N;i>=1;i--) {
+			System.out.println(i);
+		}
+
+	}
+
+}
