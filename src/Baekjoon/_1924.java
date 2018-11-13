eeb9374eec91f28db4b023e207675824da9a9e76
yujin <yujin@naver.com>
2018-11-13 15:18:26 +0900

    1924

--- src/Baekjoon/_1924.java
+++ src/Baekjoon/_1924.java
@@ -4,26 +4,23 @@ import java.util.*;
 public class _1924 {
 
 	public static void main(String[] args) {
-		Scanner scanner = new Scanner(System.in);
-		
-		int month=scanner.nextInt();
-		int day = scanner.nextInt();
-		String []week = {"MON","TUE","WED","THUR","FRI","SAT","SUN"};
-		int []day2= {31,28,31,30,31,30,31,31,30,31,30,31};
-		String result=null;
+        Scanner scanner = new Scanner(System.in);
+        int month = scanner.nextInt();
+        int day = scanner.nextInt();
+		String[] week = {"MON","TUE","WED","THU","FRI","SAT","SUN"};
+		int []day2 = {31,28,31,30,31,30,31,31,30,31,30,31};
 		
 		int sum = day;
-		
-		for(int i=0; i < month-1;i++) {
+		for(int i=0;i<month-1;i++) {
 			sum += day2[i];
 		}
-		if(sum%7==0) {
+		if(sum % 7 == 0) {
 			sum = 6;
-		}else {
+		}
+		else {
 			sum = sum%7 -1;
 		}
 		System.out.println(week[sum]);
-
 	}
 
 }
